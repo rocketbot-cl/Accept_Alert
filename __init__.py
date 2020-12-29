1e4c364ba606348dd69ee7dd450e8d7d3a904127
marcerotten <marce.rotten@gmail.com>
2020-12-29 10:44:05 -0300

    add waitAlert

--- __init__.py
+++ __init__.py
@@ -39,6 +39,7 @@ module = GetParams("module")
     Resuelvo catpcha tipo reCaptchav2
 """
 if module == "AcceptAlert":
+
     web = GetGlobals('web')
     driver = web.driver_list[web.driver_actual_id]
 
@@ -68,3 +69,30 @@ if module == "AcceptAlert":
 
     except:
         PrintException()
+
+
+if module == "waitAlert":
+    import time
+
+    web = GetGlobals('web')
+    driver = web.driver_list[web.driver_actual_id]
+
+    var = True
+    seg_ = int(GetParams('seg_'))
+    var_ = GetParams('var_')
+    cont = 1
+    res_ = ""
+
+    while var and cont <= seg_:
+        time.sleep(1)
+        try:
+            alert = driver.switch_to.alert
+            var = False
+            res_ = True
+        except:
+            PrintException()
+            cont += 1
+            res_ = False
+            continue
+
+    SetVar(var_, res_)
